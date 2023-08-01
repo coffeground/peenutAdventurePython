cfc621be3360a62957853263846a35381243c403
coffeground <140676330+coffeground@users.noreply.github.com>
2023-08-01 15:10:43 +0700

    Update try try again.py

--- try try again.py
+++ try try again.py
@@ -1,10 +1,12 @@
-import playsound
+#Resources:
+#https://codereview.stackexchange.com/questions/237601/simple-python-turn-based-battle-game
+#from playsound import playsound
+
 from enum import Enum
 import sys
 import time
 
-#Resources:
-#https://codereview.stackexchange.com/questions/237601/simple-python-turn-based-battle-game
+
 
 print("""
 \\\\\\\\\\\\\\\\\\\\
@@ -19,7 +21,7 @@ Type in 'ACCEPT' to start, or 'DECLINE' to close the game.
 -------""")
 
 while(True):
-    temp = upper(input())
+    temp = input().upper()
     if temp == "ACCEPT":
         break
     elif temp == "DECLINE":
@@ -31,7 +33,11 @@ while(True):
         print("Please type either 'ACCEPT' or 'DECLINE'.")
         continue
 
-print(""" This is a text-based game. You type in things the game tells you to.
+print(""" 
+      
+///////////      
+
+This is a text-based game. You type in things the game tells you to.
 
 Objects of interest are mostly surrounded in straight brackets: "[]"
 
@@ -47,7 +53,7 @@ In this situation, you are free to choose from 1 to 3. Type in the selected numb
 I hope you understand, but if you don't there's no function to see this text again unless you restart the entire game.
 Good luck, i guess.
 
-\\\\\\\\\\\\\\\\\\\\\
+///////////
 
 (Press enter to continue with the game)
 
@@ -57,7 +63,9 @@ input()
 
 #######
 
-class chr()
+#class chr():
+
+#def distance(lng): enable choosing between miles and km in lore
 
 def playerChoose(lastOption): #choose between 1 and unspecified last option, returns option number, asks again when entered anything else
     while(True):
@@ -116,8 +124,8 @@ How will you respond?
 
     input()
 
-    print(""">"I'm Bob, the guide you hired when travelling in the land of Placeholder.
-Do you remember anything about yourself? What's your name?"""")
+    print('''>"I'm Bob, the guide you hired when you came to the land of Placeholder.
+Do you remember anything about yourself? What's your name?"''')
     
     while(True): #silly easter egg
         name = input("Enter character name: ")
@@ -142,14 +150,16 @@ Do you remember anything about yourself? What's your name?"""")
         else:
             break
     
-    print(""">"Okay, "+name+". I'm glad to see you back. We were going to a town named Placeholder when we got attacked by a flock of geese. 
-We're currently about 
->The grass around you is a bit dry, so they aren't too dirty. You brush off some dust.
+    print('>"Okay, '+name+'. I\'m glad to see you back. We were going to a town named Dublin when we got attacked by a flock of geese."')
+    print('''>"We\'re currently about 50 kilometers from any people, so I had to make do with the first aid we packed."
+>You stand up and brush yourself off. The dirt around here is a bit dry, so there's quite a bit of dust on you.
+
+    
 
 
 
 
-    (This is the fighting tutorial. Do you want to learn how to fight? This won't affect the story.")""")
+    (This is the fighting tutorial. Do you want to learn how to fight? This won't affect the story.")''')
 
     if playerY() == 0:
         print("You won or something! Congratulations on beating up some birds. You got placeholder XP!")
@@ -162,9 +172,6 @@ We're currently about
 
 
 
-
-#    playsound('https://youtu.be/YOuMxmzmxqk')
-        
 game()
 
         
